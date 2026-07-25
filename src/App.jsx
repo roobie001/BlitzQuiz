48a07574a75e2443a2ea6a913850b5720ea5401f
meloboy001 <meloboy2000@gmail.com>
2026-07-25 13:50:02 +0100

    added toast not

--- src/App.jsx
+++ src/App.jsx
@@ -604,6 +604,7 @@ export default function App() {
           </div>
         </div>
       </div>
+      {showToast && <div className="toast">✅ Score copied to clipboard!</div>}
     </div>
   );
 }
