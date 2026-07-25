d30bc00e5c12d2cbfd3cfca9d83d96ef83ffb5c5
meloboy001 <meloboy2000@gmail.com>
2026-07-25 18:04:22 +0100

    feat: play end sound when game finishes

--- src/App.jsx
+++ src/App.jsx
@@ -180,6 +180,7 @@ export default function App() {
     setGameState((s) => {
       if (s !== "playing") return s;
       setFinalScore(finalPoints + remainingTime);
+      sounds.playEnd();
       return "finished";
     });
   }
