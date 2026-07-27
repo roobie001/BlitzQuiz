f9e3dda37de609cb1fae4e0a947799caa91d9bc2
meloboy001 <meloboy2000@gmail.com>
2026-07-27 14:47:16 +0100

    feat: scaffold ChallengeLink component

---
+++ src/components/ChallengeLink.jsx
@@ -0,0 +1,7 @@
+export function ChallengeLink({ score, mode, topic }) {
+  return (
+    <div className="challenge-link-wrap">
+      <button className="challenge-btn">🎯 Challenge a Friend</button>
+    </div>
+  );
+}
